c423ed2dc6fad2658f23bb98a06e0cfff1d053bd
Kirill Galenko <kirill.galenko@awave.com>
2022-03-13 14:41:24 +0200

    custom goal records in db meow

--- src/Components/RecordGoal/RecordGoal.js
+++ src/Components/RecordGoal/RecordGoal.js
@@ -1,11 +1,55 @@
-import React from "react";
+import React, {useContext, useEffect, useState} from "react";
 import { useForm } from "react-hook-form";
 import { useParams } from "react-router-dom";
+import {Context} from "../Auth/UserContext";
+import axios from "axios";
 
 export const RecordGoal = () => {
-  const { register, handleSubmit } = useForm();
-  const onSubmit = data => console.log(data);
   const {recordId} = useParams();
+  const { register, handleSubmit } = useForm();
+  const [activeUser] = useContext(Context);
+  const [title, setTitle] = useState("Loading...");
+  const [submitButtonIsActive, setSubmitButtonIsActive] = useState(true);
+
+  let form = {
+    headers: {'Authorization': 'Bearer ' + activeUser},
+    submit: "Save",
+    getUrl: process.env.REACT_APP_DDAPI + "goal/" + recordId,
+    submitUrl:  process.env.REACT_APP_DDAPI + "goal/record/" + recordId
+  };
+  const [submitButton, setSubmitButton] = useState(form.submit);
+
+  useEffect(() => {
+
+    axios({
+      method: 'get',
+      url: form.getUrl,
+      headers: form.headers
+    }).then(res => {
+      const data = JSON.parse(res.data);
+      setTitle(data.title);
+    });
+
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  const onSubmit = data => {
+    setSubmitButtonIsActive(false);
+    setSubmitButton("Saving");
+    const headers = {
+      'Authorization': 'Bearer ' + activeUser
+    };
+
+    axios({
+      method: 'post',
+      url: form.submitUrl,
+      data: data,
+      headers: headers
+    }).then(res => {
+      if(res.data === 200 && res.status === 200){
+        window.location.assign("/");
+      }
+    });
+  };
 
   return (
     <section className="record-goal">
@@ -13,7 +57,7 @@ export const RecordGoal = () => {
       <form onSubmit={handleSubmit(onSubmit)}>
         <div className="record-goal__title-row">
           <span className="label">Add repetitions to goal</span>
-          <span className="title">[tbd] dummy</span>
+          <span className="title">{title}</span>
         </div>
         <div className="field">
           <label className="field__label" htmlFor="amount">Amount to add</label>
@@ -23,7 +67,7 @@ export const RecordGoal = () => {
 
 
         <div className="form-footer">
-          <input type="submit" value="Save" />
+          <input type="submit" disabled={!submitButtonIsActive} value={submitButton} />
         </div>
       </form>
     </section>
