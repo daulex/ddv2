cafb2eb4daaf84e7725cc2a0d49594ceb379b153
Kirill Galenko <kirill.galenko@awave.com>
2022-05-30 17:22:36 +0300

    instant feedback on simple goal record

--- src/Screens/GoalList/ButtonGoalRecordSimple.js
+++ src/Screens/GoalList/ButtonGoalRecordSimple.js
@@ -1,23 +1,25 @@
 import axios from "axios";
+import {useState} from "react";
 
-export const ButtonGoalRecordSimple = (props) => {
-    const buttonClass = parseInt(props.goal.today) === 1 ? "complete" : "incomplete";
+export const ButtonGoalRecordSimple = ({goal, setGoalStatus}) => {
+    const buttonClass = parseInt(goal.today) === 1 ? "complete" : "incomplete";
+    const [processing, setProcessing] = useState(false);
 
     const handleButtonClick = () => {
-
+        if(processing) return;
+        setProcessing(true);
+        setGoalStatus([goal.ID, buttonClass === "complete" ? "incomplete" : "complete"]);
         axios({
             method: 'post',
-            data: props.goal,
-            url: process.env.REACT_APP_DDAPI + "goal/record/" + props.goal.ID
+            data: goal,
+            url: process.env.REACT_APP_DDAPI + "goal/record/" + goal.ID
         }).then(() => {
-
-            props.setGoalStatus([props.goal.ID, buttonClass === "complete" ? "incomplete" : "complete"]);
-
+            setProcessing(false);
         })
     }
     return(
         <button className={buttonClass} onClick={handleButtonClick}>
-            <span className="goal__title">{props.goal.title}</span>
+            <span className="goal__title">{goal.title}</span>
         </button>
     );
 }
